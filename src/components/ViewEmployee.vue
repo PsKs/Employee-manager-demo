099ccd423d883e7cd96961c88851925cbaabe142
PsKs <pongsakorn.psks@gmail.com>
2018-04-22 23:17:52 +0700

    Boilerplate.

---
+++ src/components/ViewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="view-employee">
+        <h3>view Employee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "view-employee",
+    data() {
+        return {
+
+        };
+    }
+};
+</script>
