148827ec30cae47312b3e8e2270589dd9f59142f
PsKs <pongsakorn.psks@gmail.com>
2018-04-23 06:05:25 +0700

    Change timestampsInSnapshots in Firestore.

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -2,6 +2,9 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 
+const settings = { timestampsInSnapshots: true }
 const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+firebaseApp.firestore().settings(settings)
 
 export default firebaseApp.firestore()
